146a3edfe0fe6e5a122945e9a3f8ce43fe026df6
Nasim Shiroodi <92732535+Frontendnasim@users.noreply.github.com>
2024-02-13 09:10:51 +0330

    Inject the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -52,7 +52,36 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+        class="weather-forecast-icon"
+      />
+      <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+          <strong>10º</strong>
+        </div>
+        <div class="weather-forecast-temperature">8º</div>
+      </div>
+    </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
